62b358162daf8e9aa9f4e580980c8067dbb3b25b
Jo Murphy <jomurph91@gmail.com>
2017-01-26 15:48:33 -0600

    Improved web-caesar

--- caesar.py
+++ caesar.py
@@ -1,13 +1,22 @@
-def caesar(message, key):
-    result = ""
-    alpha = string.ascii_lowercase
-    for x in message:
-        temp = x
-        ind = alpha.find(x.lower())
-        if(ind >= 0):
-            temp = alpha[(ind + key) % 26]
+import string
 
-        if(x.isupper()):
-            temp = temp.upper()
-        result += temp
-    return result
+def alphabet_position(letter):
+    alpha = string.ascii_lowercase
+    return alpha.index(letter.lower())
+
+def rotate_character(char, key):
+    alpha = string.ascii_lowercase
+    if not char.isalpha():
+        return char
+    else:
+        result = (alpha[(alphabet_position(char.lower())+key) % 26])
+    if char.isupper():
+        return result.upper()
+    else:
+        return result
+
+def encrypt(text, key):
+    newtext = ""
+    for char in text:
+        newtext += rotate_character(char, key)
+    return newtext
--- main.py
+++ main.py
@@ -20,8 +20,12 @@ import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = 'Hello world!'
-        #encrypted_message = encrypted(message, 13)
-        self.response.write(message)
+        encrypted_message = caesar.encrypt(message, 13)
+
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
